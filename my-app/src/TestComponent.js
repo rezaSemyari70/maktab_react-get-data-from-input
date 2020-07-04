e3dcdc1df414db4234631e21e536f5feca1f4537
reza <rezasemyari1991@gmail.com>
2020-07-04 20:32:52 +0430

    add h1 tag to App.js & add TestComponent.js to get fullname

---
+++ my-app/src/TestComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import MyComponent  from './TestComponent';
+
+export default class TestComponent extends Component {
+
+    state ={
+        name : '',
+        firstName : '',
+        surName : ''
+    }
+
+    onSubmit =(event) =>{
+        event.preventDefault()
+        console.log(this.state);
+    }
+
+    changeHandler = event =>{
+        const {target:{value , name}} = event;
+        this.setState({[name] : value})
+    }
+
+
+    render() {      
+        console.log('render');
+          
+        return (
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <input name="firstName" type="text" value={this.state.firstName} placeholder="what's your name?" onChange={this.changeHandler}></input>
+                    <input name="surName" type="text" value={this.state.surName} placeholder="and surname?" onChange={this.changeHandler}></input>
+                    <button>Send</button>
+                </form>
+                {`${this.state.firstName} ${this.state.surName}`}
+            </div>
+        )
+    }
+}
